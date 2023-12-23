4ba5aa98fe6f500aee6dc3de2e8df2a8c29651e8
Ojieide Egbomondion <ojieide@yahoo.com>
2023-12-24 00:57:25 +0100

    Implementing the create post feature

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,8 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -25,5 +25,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['QUERY_KEYS.GET_RECENT_POSTS']
+            })       
+        },
     })
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { Models } from "appwrite";
+
 export type IContextType = {
   user: IUser;
   isLoading: boolean;
@@ -5,53 +7,62 @@ export type IContextType = {
   isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
   checkAuthUser: () => Promise<boolean>;
-}
+};
 
 export type INavLink = {
-    imgURL: string;
-    route: string;
-    label: string;
-  };
+  imgURL: string;
+  route: string;
+  label: string;
+};
   
-  export type IUpdateUser = {
-    userId: string;
-    name: string;
-    bio: string;
-    imageId: string;
-    imageUrl: URL | string;
-    file: File[];
-  };
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
   
-  export type INewPost = {
-    userId: string;
-    caption: string;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
   
-  export type IUpdatePost = {
-    postId: string;
-    caption: string;
-    imageId: string;
-    imageUrl: URL;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
   
-  export type IUser = {
-    id: string;
-    name: string;
-    username: string;
-    email: string;
-    imageUrl: string;
-    bio: string;
-  };
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
   
-  export type INewUser = {
-    name: string;
-    email: string;
-    username: string;
-    password: string;
-  };
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
+
+export type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+};
+
+export type PostFormProps = {
+  post?: Models.Document; 
+};
